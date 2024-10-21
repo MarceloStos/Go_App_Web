1176f4e043d987bfd70e57bea443547ee7e6a3d7
Marcelo <marcelostos.dev@gmail.com>
2024-10-21 17:59:12 -0300

    Pronto

--- routes/routes.go
+++ routes/routes.go
@@ -12,7 +12,20 @@ func CarregaRotas() {
 	//http.HandleFunc("/deletar", controllers.Deletar)
 	//http.HandleFunc("/editar", controllers.Editar)
 	http.HandleFunc("/update", controllers.Atualizar)
+
 	http.HandleFunc("/matriculas", controllers.IndexMatriculas)
+	http.HandleFunc("/matriculas/adicionar", controllers.AdicionarMatriculas)
+	http.HandleFunc("/matriculas/inserir", controllers.InserirMatriculas)
+	http.HandleFunc("/matriculas/editar", controllers.EditarMatriculas)
+	http.HandleFunc("/matriculas/atualizar", controllers.AtualizarMatriculas)
+	http.HandleFunc("/matriculas/deletar", controllers.DeletarMatriculas)
+
+	http.HandleFunc("/alunos", controllers.IndexAlunos)
+	http.HandleFunc("/alunos/adicionar", controllers.AdicionarAlunos)
+	http.HandleFunc("/alunos/inserir", controllers.InserirAlunos)
+	http.HandleFunc("/alunos/editar", controllers.EditarAlunos)
+	http.HandleFunc("/alunos/atualizar", controllers.AtualizarAlunos)
+	http.HandleFunc("/alunos/deletar", controllers.DeletarAlunos)
 
 	http.HandleFunc("/disciplinas", controllers.IndexDisciplinas)
 	http.HandleFunc("/disciplinas/adicionar", controllers.AdicionarDisciplinas)
